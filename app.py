fc398dbd3ac588370b8b401f1d27361018a801cf
valts <124685208+va1ts@users.noreply.github.com>
2025-02-06 14:20:38 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ import logging
 import atexit
 from flask import Flask, render_template, request, redirect, url_for, session
 from werkzeug.security import generate_password_hash, check_password_hash
-from gpiozero import OutputDevice, GPIOZeroError
+from gpiozero import OutputDevice, GPIOZeroError, Device
+from gpiozero.pins.native import NativeFactory
 
 # Initialize Flask app
 app = Flask(__name__)
@@ -12,6 +13,9 @@ app.secret_key = 'your_secret_key'
 
 # Logging configuration
 logging.basicConfig(level=logging.DEBUG)
+
+# Set the default pin factory to NativeFactory to avoid conflicts
+Device.pin_factory = NativeFactory()
 
 # GPIO Setup
 fan_pin = 18
